83d542f26a79e58e8d36eb8753c2c85dea58a7cb
3epiossi <3epiossi@gmail.com>
2025-04-25 15:53:52 +0800

    add debug log

--- esp32_csi/dataset.py
+++ esp32_csi/dataset.py
@@ -1,204 +1,125 @@
 import os
-
+import argparse
+import logging
 import torch
 import numpy as np
 import pandas as pd
-
 from torch.utils.data import Dataset, DataLoader
-from collections import defaultdict
+from tqdm import tqdm
 
 
-# from data_calibration import calibrate_amplitude_custom, calibrate_phase, calibrate_amplitude, dwn_noise, hampel
+CLASS_FOLDER = os.path.join(os.path.dirname(__file__), "data")
 
-CLASS_FOLDER = os.path.join(os.path.dirname(__file__),"data")
-SUBCARRIES_NUM = 38
-WINDOWS_SIZE = 50
-PHASE_MAX, PHASE_MIN =  3.1415, -3.1415 #3.1389
-AMP_MIN, AMP_MAX = 0.0, 577.6582
+def read_csi_from_csv(path_to_csv, debug=False):
+    try:
+        data = pd.read_csv(path_to_csv, header=None).values
+    except pd.errors.EmptyDataError:
+        if debug:
+            logger.info(f"Empty CSV file: {path_to_csv}")
+        return np.empty((0, 0)), np.empty((0, 0))
 
-class_to_idx = defaultdict(int)
-class_to_idx["one"] = 1
-class_to_idx["two"] = 2
-def read_csi_from_csv(path_to_csv):
-    """
-    Read csi data(amplitude, phase) from .csv data
-
-    :param path_to_csv: string
-    :param is_five_hhz: boolean
-    :param antenna_pairs: integer
-    :return: (amplitudes, phases) => (np.array of shape(data len, num_of_subcarriers * antenna_pairs),
-                                     np.array of shape(data len, num_of_subcarriers * antenna_pairs))
-    """
-
-    data = pd.read_csv(path_to_csv, header=None).values
-    amplitudes = data[:,0::2]
+    amplitudes = data[:, 0::2]
     phases = data[:, 1::2]
-    
+
+    if debug:
+        logger.debug(f"Loaded CSV: {path_to_csv}")
+        logger.debug(f"Shape - Amplitudes: {amplitudes.shape}, Phases: {phases.shape}")
+
     return amplitudes, phases
 
 
-# def read_labels_from_csv(path_to_csv):
-#     """
-#     Read labels(human activities) from csv file
 
-#     :param path_to_csv: string
-#     :return: labels, np.array of shape(data_len, 1)
-#     """
+def read_all_csv_from_class(class_names, window_size, debug=False):
+    final_amplitudes, final_phases, final_labels = [], [], []
 
-#     data = pd.read_csv(path_to_csv, header=None).values
-#     labels = data[:, 1]
-
-#     return labels
-
-
-def read_all_csv_from_class(class_names):
-    """
-    Read csi and labels data from all folders in the dataset
-
-    :return: amplitudes, phases, labels all of shape (data len, num of subcarriers)
-    """
-    
-    final_amplitudes, final_phases, final_labels = np.empty((0, WINDOWS_SIZE, SUBCARRIES_NUM)), \
-                                                   np.empty((0, WINDOWS_SIZE, SUBCARRIES_NUM)), \
-                                                   np.empty((0))
-                                                   
-    print("Amplitudes shape:", final_amplitudes.shape)
-    print("Phases shape:", final_phases.shape)
     for label, class_name in enumerate(class_names):
-        class_dir_name = os.path.join(CLASS_FOLDER, class_name)
-        for csv_name in os.listdir(class_dir_name):
-            amplitudes, phases = read_csi_from_csv(os.path.join(class_dir_name, csv_name))
-            # print("Amplitudes shape:", amplitudes.shape)
-            # print("Phases shape:", phases.shape)
-            if amplitudes.shape[0] < WINDOWS_SIZE or phases.shape[0] < WINDOWS_SIZE:
-                continue
-            amplitudes, phases = amplitudes[:WINDOWS_SIZE], phases[:WINDOWS_SIZE]  # fix the bug with the last element
+        class_dir = os.path.join(CLASS_FOLDER, class_name)
+        if not os.path.isdir(class_dir):
+            if debug:
+                logger.debug(f"Skipping non-directory: {class_dir}")
+            continue
 
-            final_amplitudes = np.concatenate((final_amplitudes, np.array((amplitudes,))))
-            final_phases = np.concatenate((final_phases, np.array((phases,))))
-            final_labels = np.append(final_labels, label)
-            print("Amplitudes shape:", final_amplitudes.shape)
-            print("Phases shape:", final_phases.shape)
+        csv_files = [f for f in os.listdir(class_dir) if f.endswith('.csv')]
+        if debug:
+            logger.debug(f"Reading class '{class_name}' with label {label} ({len(csv_files)} files)")
+
+        for csv_name in csv_files:
+            csv_path = os.path.join(class_dir, csv_name)
+            amplitudes, phases = read_csi_from_csv(csv_path, debug)
+
+            if amplitudes.shape[0] < window_size or phases.shape[0] < window_size:
+                if debug:
+                    logger.debug(f"Skipping short sequence: {csv_name}")
+                continue
+
+            final_amplitudes.append(amplitudes[:window_size])
+            final_phases.append(phases[:window_size])
+            final_labels.append(label)
+
+    if debug:
+        logger.debug(f"Total samples loaded: {len(final_labels)}")
 
     return final_amplitudes, final_phases, final_labels
 
 
 
 class CSIDataset(Dataset):
-    def __init__(self, class_names, window_size=32, step=1):
-        self.amplitudes, self.phases, self.labels = read_all_csv_from_class(class_names)
-        # print("Amplitudes shape:", self.amplitudes.shape)
-        # print("Phases shape:", self.phases.shape)
-        # self.amplitudes = calibrate_amplitude(self.amplitudes)
-        # self.phases = calibrate_phase(self.phases)
+    def __init__(self, class_names, window_size, debug=False):
+        self.window_size = window_size
+        self.amplitudes, self.phases, self.labels = read_all_csv_from_class(class_names, window_size, debug)
+        self.features = [
+            np.concatenate([amp, phase], axis=1)
+            for amp, phase in zip(self.amplitudes, self.phases)
+        ]
 
-        self.amplitudes = np.clip(self.amplitudes, AMP_MIN, AMP_MAX)
-        self.phases = np.clip(self.phases, PHASE_MIN, PHASE_MAX)
+        if debug:
+            logger.debug(f"Dataset created with {len(self.labels)} samples")
 
-        self.window = window_size
-        if window_size == -1:
-            self.window = self.labels.shape[0] - 1
-
-        self.step = step
-    
     def __getitem__(self, index):
-        return np.concatenate((self.amplitudes[index], self.phases[index]), axis=1), class_to_idx[self.labels[index]]
+        feature = torch.tensor(self.features[index], dtype=torch.float32)
+        label = torch.tensor(self.labels[index], dtype=torch.long)
+        return feature, label
+
     def __len__(self):
-        return self.labels.shape[0]
+        return len(self.labels)
 
-
-# class CSIDataset(Dataset):
-#     """CSI Dataset."""
-
-#     def __init__(self, csv_files, window_size=32, step=1):
-#         from sklearn import decomposition
-
-#         self.amplitudes, self.phases, self.labels = read_all_data_from_files(csv_files)
-
-#         self.amplitudes = calibrate_amplitude(self.amplitudes)
-
-#         pca = decomposition.PCA(n_components=3)
-
-#         # self.phases[:, 0 * SUBCARRIES_NUM_FIVE_HHZ:1 * SUBCARRIES_NUM_FIVE_HHZ] = calibrate_amplitude(calibrate_phase(
-#         #     self.phases[:, 0 * SUBCARRIES_NUM_FIVE_HHZ:1 * SUBCARRIES_NUM_FIVE_HHZ]))
-#         # self.phases[:, 1 * SUBCARRIES_NUM_FIVE_HHZ:2 * SUBCARRIES_NUM_FIVE_HHZ] = calibrate_amplitude(calibrate_phase(
-#         #     self.phases[:, 1 * SUBCARRIES_NUM_FIVE_HHZ:2 * SUBCARRIES_NUM_FIVE_HHZ]))
-#         # self.phases[:, 2 * SUBCARRIES_NUM_FIVE_HHZ:3 * SUBCARRIES_NUM_FIVE_HHZ] = calibrate_amplitude(calibrate_phase(
-#         #     self.phases[:, 2 * SUBCARRIES_NUM_FIVE_HHZ:3 * SUBCARRIES_NUM_FIVE_HHZ]))
-#         # self.phases[:, 3 * SUBCARRIES_NUM_FIVE_HHZ:4 * SUBCARRIES_NUM_FIVE_HHZ] = calibrate_amplitude(calibrate_phase(
-#         #     self.phases[:, 3 * SUBCARRIES_NUM_FIVE_HHZ:4 * SUBCARRIES_NUM_FIVE_HHZ]))
-
-#         self.amplitudes_pca = []
-
-#         data_len = self.phases.shape[0]
-#         for i in range(self.phases.shape[1]):
-#             # self.phases[:data_len, i] = dwn_noise(hampel(self.phases[:, i]))[:data_len]
-#             self.amplitudes[:data_len, i] = dwn_noise(hampel(self.amplitudes[:, i]))[:data_len]
-
-#         for i in range(4):pca.fit_transform(
-#             self.amplitudes_pca.append(
-#                 self.amplitudes[:, i * SUBCARRIES_NUM_FIVE_HHZ:(i + 1) * SUBCARRIES_NUM_FIVE_HHZ]))
-#         self.amplitudes_pca = np.array(self.amplitudes_pca)
-#         self.amplitudes_pca = self.amplitudes_pca.reshape((self.amplitudes_pca.shape[1], self.amplitudes_pca.shape[0] * self.amplitudes_pca.shape[2]))
-
-#         self.label_keys = list(set(self.labels))
-#         self.class_to_idx = {
-#             "standing": 0,
-#             "walking": 1,
-#             "get_down": 2,
-#             "sitting": 3,
-#             "get_up": 4,
-#             "lying": 5,
-#             "no_person": 6
-#         }
-#         self.idx_to_class = {v: k for k, v in self.class_to_idx.items()}
-
-#         self.window = window_size
-#         if window_size == -1:
-#             self.window = self.labels.shape[0] - 1
-
-#         self.step = step
-
-#     def __getitem__(self, idx):
-#         if self.window == 0:
-#             return np.append(self.amplitudes[idx], self.phases[idx]), self.class_to_idx[
-#                 self.labels[idx + self.window - 1]]
-
-#         idx = idx * self.step
-#         all_xs, all_ys = [], []
-#         # idx = idx * self.window
-
-#         for index in range(idx, idx + self.window):
-#             all_xs.append(np.append(self.amplitudes[index], self.amplitudes_pca[index]))
-#             # all_ys.append(self.class_to_idx[self.labels[index]])
-
-#         return np.array(all_xs), self.class_to_idx[self.labels[idx + self.window - 1]]
-#         # return np.array(all_xs), np.array(all_ys)
-
-#     def __len__(self):
-#         # return self.labels.shape[0] // self.window
-#         # return (self.labels.shape[0] - self.window
-#         return int((self.labels.shape[0] - self.window) // self.step) + 1
 
 
 if __name__ == '__main__':
-    val_dataset = CSIDataset(class_to_idx.keys())
+    parser = argparse.ArgumentParser(description="CSI Data Loader with configurable subcarrier and sequence length")
+    parser.add_argument('--window', type=int, default=84, help='Window length (default: 84)')
+    parser.add_argument('--debug', action='store_true', help='Enable debug logging')
+    parser.add_argument('--logfile', type=str, default='dataset.log', help='Log file name (default: dataset.log)')
+    parser.add_argument('--output-dir', type=str, default=os.path.join(os.path.dirname(__file__), "output"), help='Path to output folder')
+    args = parser.parse_args()
 
-    dl = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=1)
+    window_size = args.window
+    debug = args.debug
+    log_filename = args.logfile
 
-    for i in dl:
-        print(i[0].shape)
-        print(i[1].shape)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
 
-        break
+    console_handler = logging.StreamHandler()
+    console_formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    console_handler.setFormatter(console_formatter)
 
-    print(val_dataset[0])
-# if __name__ == '__main__':
-#     # Example usage
-#     class_names = os.listdir(CLASS_FOLDER)
-#     print(class_names)
-#     amplitudes, phases= read_all_csv_from_class(class_names)
-#     print("Amplitudes shape:", amplitudes.shape)
-#     print("Phases shape:", phases.shape)
-#     print("Amplitudes:", amplitudes)
-#     print("Phases:", phases)
+    logfile_dir = os.path.join(args.output_dir, 'logs')
+    if not os.path.exists(logfile_dir):
+        os.makedirs(logfile_dir)
+    file_handler = logging.FileHandler(os.path.join(logfile_dir, args.logfile), mode='w')
+    file_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(file_formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    logger.info(f"window_size: {window_size}, Log file: {log_filename}")
+
+    class_names = [d for d in os.listdir(CLASS_FOLDER) if os.path.isdir(os.path.join(CLASS_FOLDER, d))]
+    dataset = CSIDataset(class_names, window_size, debug=debug)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    for i, (x_batch, y_batch) in tqdm(enumerate(dataloader), total=len(dataloader), desc="Loading dataset"):
+        logger.info(f"Batch {i} - x_batch shape: {x_batch.shape}, y_batch: {y_batch.item()}")
+
